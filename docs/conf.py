06b5a9eb5db4787ab8e2f81fc0c55cf0728e6b87
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2025-08-14 16:31:05 +0200

    t

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx_needs',
     'sphinxcontrib.plantuml',
-    'sphinxcontrib.test_reports',
+    #'sphinxcontrib.test_reports',
     'sphinxcontrib.collections',
     'sphinxcontrib.jquery', # https://github.com/sphinx-contrib/jquery
     'sphinx_preview',
